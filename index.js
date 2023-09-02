cd25e37cf374c397a7daa3657df7b572ba735af9
Hemel Bourhan <hemelbourhan@gmailgit config --global user.email Bourhan>
2023-09-02 20:34:40 +0600

    load all the data

--- index.js
+++ index.js
@@ -1,18 +1,43 @@
-const  request  = require("express");
+const request = require("express");
 const app = request();
-const cors = require('cors');
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-
-const categories = require('./data/categories.json');
-app.get('/categories', (req,res)=>{
-    res.send(categories);
-})
-
-app.get('/', (req, res)=>{
-    res.send('News is Coming Soon..') 
+// Only categories of news
+const categories = require("./data/categories.json");
+app.get("/categories", (req, res) => {
+  res.send(categories);
 });
- app.listen(port, ()=>{
-    console.log(`Latest News is Coming on Port : ${port}`);    
- })
+
+app.get("/categories/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  // console.log(id);
+  if (id === 0) {
+    res.send(news);
+  } else {
+    const categoryNews = news.filter((n) => parseInt(n.category_id) === id);
+    res.send(categoryNews);
+  }
+});
+
+// to get all news
+const news = require("./data/news.json");
+app.get("/news", (req, res) => {
+  res.send(news);
+});
+
+// to get specific news by id
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const selectedNews = news.find((n) => n._id === id);
+  res.send(selectedNews);
+});
+
+app.get("/", (req, res) => {
+  res.send("News is Coming Soon..");
+});
+app.listen(port, () => {
+  console.log(`Latest News is Coming on Port : ${port}`);
+});
